0863c77a704ba2f0f01a062fa813e19e948a7708
Simon Zelazny <zelazny@mailbox.org>
2018-04-02 19:19:28 -0700

    Golf

--- grep.exs
+++ grep.exs
@@ -1,14 +1,11 @@
 #!/usr/bin/env elixir
-
 defmodule Grep do
   def main(args) do
     case args do
       [pattern] ->
-        IO.stream(:stdio, :line)
-        |> process_one(Regex.compile!(pattern))
+        IO.stream(:stdio, :line) |> process_one(Regex.compile!(pattern))
       [pattern, path] ->
-        File.stream!(path, [:read], :line)
-        |> process_one(Regex.compile!(pattern))
+        File.stream!(path, [], :line) |> process_one(Regex.compile!(pattern))
       [pattern | paths] ->
         Enum.map(paths, fn p -> {p, File.read!(p)} end)
         |> process_many(Regex.compile!(pattern))
@@ -17,25 +14,19 @@ defmodule Grep do
 
   defp process_one(stream, re) do
     Stream.filter(stream, &Regex.match?(re, &1))
-    |> Stream.map(&IO.write/1)
-    |> Stream.run()
+    |> Enum.each(&IO.write/1)
   end
 
   defp process_many(annotated_files, re) do
     Task.async_stream(annotated_files,
-      fn {annot, contents} ->
-        {annot, find_matches(contents, re)}
-      end)
-    |> Enum.to_list()
+      fn {annot, txt} -> {annot, find_matches(txt, re)} end)
     |> Enum.each(&print_matches/1)
   end
 
   defp find_matches(blob, re) do
-    String.split(blob, "\n")
-    |> Enum.filter(&Regex.match?(re, &1))
+    String.split(blob, "\n") |> Enum.filter(&Regex.match?(re, &1))
   end
 
-  defp print_matches({:ok, {_, []}}), do: :ok
   defp print_matches({:ok, {path, matches}}) do
     Enum.each(matches, fn match ->
       IO.write(:stdio, "#{path}:#{match}\n")
