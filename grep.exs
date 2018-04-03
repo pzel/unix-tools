ed90c6975b85e48b3d8c48e28b8e8484a0f0d1d6
Simon Zelazny <zelazny@mailbox.org>
2018-04-02 19:15:49 -0700

    Compile regex only once

--- grep.exs
+++ grep.exs
@@ -5,35 +5,34 @@ defmodule Grep do
     case args do
       [pattern] ->
         IO.stream(:stdio, :line)
-        |> process_one(pattern)
+        |> process_one(Regex.compile!(pattern))
       [pattern, path] ->
         File.stream!(path, [:read], :line)
-        |> process_one(pattern)
+        |> process_one(Regex.compile!(pattern))
       [pattern | paths] ->
         Enum.map(paths, fn p -> {p, File.read!(p)} end)
-        |> process_many(pattern)
+        |> process_many(Regex.compile!(pattern))
     end
   end
 
-  defp process_one(stream, pattern) do
-    stream
-    |> Stream.filter(&pattern_matches?(pattern, &1))
+  defp process_one(stream, re) do
+    Stream.filter(stream, &Regex.match?(re, &1))
     |> Stream.map(&IO.write/1)
     |> Stream.run()
   end
 
-  defp process_many(annotated_files, pattern) do
+  defp process_many(annotated_files, re) do
     Task.async_stream(annotated_files,
       fn {annot, contents} ->
-        {annot, find_matches(contents, pattern)}
+        {annot, find_matches(contents, re)}
       end)
     |> Enum.to_list()
     |> Enum.each(&print_matches/1)
   end
 
-  defp find_matches(blob, pattern) do
+  defp find_matches(blob, re) do
     String.split(blob, "\n")
-    |> Enum.filter(&pattern_matches?(pattern, &1))
+    |> Enum.filter(&Regex.match?(re, &1))
   end
 
   defp print_matches({:ok, {_, []}}), do: :ok
@@ -42,11 +41,6 @@ defmodule Grep do
       IO.write(:stdio, "#{path}:#{match}\n")
     end)
   end
-
-  defp pattern_matches?(pattern, subject) do
-    Regex.match?(Regex.compile!(pattern), subject)
-  end
-
 end
 
 System.argv() |> Grep.main()
